92e821e1f51c9af1aaf901a1800dfcea93b2809b
adidoank <42825332+adidoank@users.noreply.github.com>
2018-10-13 21:31:45 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+//masukan config token yg terdapat pada link di packet captur
+// jangan hapus tanda kutip(" ") 
+
+$access_token = "6d8c3c99-b5a1-4b0b-972d-74602c116be4";
+
+// kamu bisa membuat banyak config untuk beberapa akun.
